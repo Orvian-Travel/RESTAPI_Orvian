24e998f8c9587be1d2835ef696e9a7a4f87f7a82
Leonardo Lima <leo2342005@gmail.com>
2025-07-08 12:05:36 -0300

    docs: adicionando documentação ao UsuarioController.

--- src/main/java/com/orvian/travelapi/controller/impl/UserControllerImpl.java
+++ src/main/java/com/orvian/travelapi/controller/impl/UserControllerImpl.java
@@ -8,6 +8,12 @@ import com.orvian.travelapi.controller.dto.UserSearchResultDTO;
 import com.orvian.travelapi.domain.model.User;
 import com.orvian.travelapi.mapper.UserMapper;
 import com.orvian.travelapi.service.impl.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,12 +30,20 @@ import java.util.UUID;
 @RequestMapping("/api/v1/users")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "User Management", description = "Operations related to user management")
 public class UserControllerImpl implements GenericController {
 
     private final UserServiceImpl userService;
     private final UserMapper userMapper;
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Creates a new user with the provided details.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "409", description = "User with the same email already exists", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
     public ResponseEntity<Void> create(@RequestBody @Valid CreateUserDTO dto) {
         User user = userMapper.toEntity(dto);
         log.info("Creating user with email: {}", user.getEmail());
@@ -40,6 +54,14 @@ public class UserControllerImpl implements GenericController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing user", description = "Updates the details of an existing user identified by ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "409", description = "User with the same details already exists", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
     public ResponseEntity<Object> update(@PathVariable UUID id, @RequestBody @Valid UpdateUserDTO dto) {
         Optional<User> userOptional = userService.findById(id);
 
@@ -64,6 +86,11 @@ public class UserControllerImpl implements GenericController {
     }
 
     @GetMapping
+    @Operation(summary = "Find all users", description = "Retrieves a list of all registered users.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Users retrieved successfully"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
     public ResponseEntity<List<User>> findAll() {
         log.info("Fetching all users");
         List<User> users = userService.findAll();
@@ -72,6 +99,12 @@ public class UserControllerImpl implements GenericController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find a user by ID", description = "Retrieves a user identified by ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
     public ResponseEntity<UserSearchResultDTO> findById(@PathVariable UUID id) {
 
         return userService.findById(id)
@@ -86,6 +119,12 @@ public class UserControllerImpl implements GenericController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a user", description = "Deletes a user identified by ID.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ResponseErrorDTO.class)))
+    })
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         Optional<User> userOptional = userService.findById(id);
 
