42c96aab920eefee723e105fa595282fa703073b
diegodemello <diegodemello027@gmail.com>
2025-07-12 14:36:11 -0300

    feat: adiciona método para converter DTO de criação de pacotes.

--- src/main/java/com/orvian/travelapi/mapper/TravelPackageMapper.java
+++ src/main/java/com/orvian/travelapi/mapper/TravelPackageMapper.java
@@ -1,5 +1,6 @@
 package com.orvian.travelapi.mapper;
 
+import com.orvian.travelapi.controller.dto.CreateTravelPackageDTO;
 import com.orvian.travelapi.controller.dto.PackageSearchResultDTO;
 import com.orvian.travelapi.domain.model.TravelPackage;
 import org.mapstruct.Mapper;
@@ -8,7 +9,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TravelPackageMapper {
+    TravelPackage toTravelPackage(CreateTravelPackageDTO dto);
     PackageSearchResultDTO toPackageSearchResultDTO(TravelPackage entity);
-
     List<PackageSearchResultDTO> toPackageSearchResultDTOList(List<TravelPackage> entities);
 }
