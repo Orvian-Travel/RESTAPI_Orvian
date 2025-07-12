00785db8e8bbe962b9895aa3e8c8375b81f1e9a9
diegodemello <diegodemello027@gmail.com>
2025-07-12 13:51:02 -0300

    feat: cria a service com busca de pacotes e tratamento de exceção.

---
+++ src/main/java/com/orvian/travelapi/service/impl/PackageServiceImpl.java
@@ -0,0 +1,50 @@
+package com.orvian.travelapi.service.impl;
+
+import com.orvian.travelapi.controller.dto.PackageSearchResultDTO;
+import com.orvian.travelapi.domain.model.TravelPackage;
+import com.orvian.travelapi.domain.repository.TravelPackageRepository;
+import com.orvian.travelapi.mapper.TravelPackageMapper;
+import com.orvian.travelapi.service.TravelPackageService;
+import com.orvian.travelapi.service.exception.NoPackageFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PackageServiceImpl implements TravelPackageService {
+    private final TravelPackageRepository travelPackageRepository;
+    private final TravelPackageMapper travelPackageMapper;
+
+    @Override
+    public List<PackageSearchResultDTO> findAll() {
+        List<TravelPackage> packages = travelPackageRepository.findAll();
+        if(packages == null || packages.isEmpty()) {
+            throw new NoPackageFoundException("No travel packages found.");
+        }
+        return travelPackageMapper.toPackageSearchResultDTOList(packages);
+    }
+
+    @Override
+    public Optional<TravelPackage> findById(UUID uuid) {
+        return Optional.empty();
+    }
+
+    @Override
+    public TravelPackage create(TravelPackage entity) {
+        return null;
+    }
+
+    @Override
+    public TravelPackage update(TravelPackage entity) {
+        return null;
+    }
+
+    @Override
+    public void delete(UUID uuid) {
+
+    }
+}
