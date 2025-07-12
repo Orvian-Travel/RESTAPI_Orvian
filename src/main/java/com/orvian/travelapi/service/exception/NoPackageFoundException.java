c6da2719b8b543e44c7d890ecdeac79999eb61c1
diegodemello <diegodemello027@gmail.com>
2025-07-12 13:52:18 -0300

    feat: cria uma exceção para ausência de pacotes.

---
+++ src/main/java/com/orvian/travelapi/service/exception/NoPackageFoundException.java
@@ -0,0 +1,7 @@
+package com.orvian.travelapi.service.exception;
+
+public class NoPackageFoundException extends RuntimeException {
+    public NoPackageFoundException(String message) {
+        super(message);
+    }
+}
