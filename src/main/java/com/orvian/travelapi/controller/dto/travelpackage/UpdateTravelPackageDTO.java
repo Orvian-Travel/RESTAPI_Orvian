23647d377df9393120c7fab8bb4319a437762974
Leonardo Lima <leo2342005@gmail.com>
2025-07-16 21:32:25 -0300

    refactor: alterando para que possa ser possível dar update parcial em um Pacote, adicionando no MapStruct para ele ignorar os valores nulos.

--- src/main/java/com/orvian/travelapi/controller/dto/travelpackage/UpdateTravelPackageDTO.java
+++ src/main/java/com/orvian/travelapi/controller/dto/travelpackage/UpdateTravelPackageDTO.java
@@ -8,15 +8,12 @@ import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public record UpdateTravelPackageDTO(
-        @NotBlank
         String title,
-        @NotBlank
         String description,
-        @NotBlank
         String destination,
         @Min(value = 1, message = "Duration must be at least 1 day")
         int duration,
-        @NotNull @DecimalMin(value = "10.0", message = "Price must be at least 10.0")
+        @DecimalMin(value = "10.0", message = "Price must be at least 10.0")
         BigDecimal price,
         @Min(value = 1, message = "Max people must be at least 1")
         int maxPeople
--- src/main/java/com/orvian/travelapi/mapper/TravelPackageMapper.java
+++ src/main/java/com/orvian/travelapi/mapper/TravelPackageMapper.java
@@ -4,14 +4,21 @@ import com.orvian.travelapi.controller.dto.travelpackage.CreateTravelPackageDTO;
 import com.orvian.travelapi.controller.dto.travelpackage.PackageSearchResultDTO;
 import com.orvian.travelapi.controller.dto.travelpackage.UpdateTravelPackageDTO;
 import com.orvian.travelapi.domain.model.TravelPackage;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TravelPackageMapper {
+
     TravelPackage toTravelPackage(CreateTravelPackageDTO dto);
+
     PackageSearchResultDTO toPackageSearchResultDTO(TravelPackage entity);
+
     List<PackageSearchResultDTO> toPackageSearchResultDTOList(List<TravelPackage> entities);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     TravelPackage toUpdateTravelPackage(UpdateTravelPackageDTO dto);
 }
