e1ab94d3913d27b6c24c402c1644177fee8ff062
Leonardo Lima <leo2342005@gmail.com>
2025-07-23 15:51:54 -0300

    fix: alterando para UUID no RatingRepository.

--- src/main/java/com/orvian/travelapi/domain/repository/RatingRepository.java
+++ src/main/java/com/orvian/travelapi/domain/repository/RatingRepository.java
@@ -3,5 +3,7 @@ package com.orvian.travelapi.domain.repository;
 import com.orvian.travelapi.domain.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RatingRepository extends JpaRepository<Rating, Long> {
+import java.util.UUID;
+
+public interface RatingRepository extends JpaRepository<Rating, UUID> {
 }
