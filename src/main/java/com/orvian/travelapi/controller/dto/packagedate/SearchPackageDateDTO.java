b6a6c3bff0fdc3643fb367e21fd3070d7880e304
diegodemello <diegodemello027@gmail.com>
2025-07-24 10:12:28 -0300

    feat: cria o DTO de pesquisa para a entidade PackageDate.

---
+++ src/main/java/com/orvian/travelapi/controller/dto/packagedate/SearchPackageDateDTO.java
@@ -0,0 +1,12 @@
+package com.orvian.travelapi.controller.dto.packagedate;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record SearchPackageDateDTO(
+        UUID id,
+        LocalDate startDate,
+        LocalDate endDate,
+        int qtd_available
+) {
+}
