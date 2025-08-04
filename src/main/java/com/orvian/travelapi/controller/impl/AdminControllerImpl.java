215ccd54d2568f1727c86d5e9d8814a49abd1b3a
Vitor Rodrigues <vitorrdesouza022@gmail.com>
2025-08-04 14:22:23 -0300

    retirando imports não usados

--- src/main/java/com/orvian/travelapi/controller/impl/AdminControllerImpl.java
+++ src/main/java/com/orvian/travelapi/controller/impl/AdminControllerImpl.java
@@ -1,8 +1,6 @@
 package com.orvian.travelapi.controller.impl;
 
 import com.orvian.travelapi.controller.dto.admin.DashboardDTO;
-import com.orvian.travelapi.controller.dto.admin.WeekRatingDTO;
-import com.orvian.travelapi.controller.dto.travelpackage.PaymentByPackageDTO;
 import com.orvian.travelapi.domain.repository.RatingRepository;
 import com.orvian.travelapi.domain.repository.TravelPackageRepository;
 import com.orvian.travelapi.domain.repository.UserRepository;
@@ -12,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
